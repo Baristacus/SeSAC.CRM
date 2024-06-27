b42f8b233410ec409078e109d91dade1700e1d02
Baristacus <chic.igel@gmail.com>
2024-06-28 02:37:22 +0900

    메인페이지 내용 추가

--- crm/views/main.py
+++ crm/views/main.py
@@ -1,8 +1,23 @@
 from flask import Blueprint, render_template
+from crm.models import User, Order, Store, Item, OrderItem
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
 @bp.route("/")
 def index():
-    return render_template("main/index.jinja2")
+
+    user_count = User.query.count()
+    order_count = Order.query.count()
+    store_count = Store.query.count()
+    item_count = Item.query.count()
+    orderitem_count = OrderItem.query.count()
+
+    return render_template(
+        "main/index.jinja2",
+        user_count=user_count,
+        store_count=store_count,
+        item_count=item_count,
+        order_count=order_count,
+        orderitem_count=orderitem_count,
+    )
